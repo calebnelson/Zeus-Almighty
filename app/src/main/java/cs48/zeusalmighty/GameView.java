2427ee99d460dc29ce9a167f031e1d7ee607eb6a
Jared <jared.bradley.jensen@gmail.com>
2015-02-09 22:19:01 -0800

    all features added, fixed memory leak

--- app/src/main/java/cs48/zeusalmighty/GameView.java
+++ app/src/main/java/cs48/zeusalmighty/GameView.java
@@ -1,39 +1,44 @@
 package cs48.zeusalmighty;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 public class GameView extends SurfaceView {
     private Bitmap bmp;
     private SurfaceHolder holder;
     private GameThread gameLoopThread;
-    private List<Sprite> sprites = new ArrayList<Sprite>();
+    private List<Peasant> peasants = new ArrayList<Peasant>();
     private Lightning lightning;
     private long lastClick;
     private Background background;
     private Main activity;
     private Paint paint;
     private int highScore, currentScore;
+    private int positionCounter = 1000;
+    //the lower the number, the harder it is. must be between 0 and 1
+    public double difficultyDecider = 0.95;
+    private double timeTracker = 15000;
+    private int speed = 20;
+    private double startTime = System.currentTimeMillis();
+    private double systemTime = System.currentTimeMillis();
 
     public GameView(Context context) {
         super(context);
         activity = (Main) context;
         paint = new Paint();
-        paint.setColor(Color.BLACK);
+        paint.setColor(Color.BLUE);
         paint.setTextSize(100);
         highScore = activity.getHighScore();
         gameLoopThread = new GameThread(this);
@@ -57,7 +62,6 @@ public class GameView extends SurfaceView {
             public void surfaceCreated(SurfaceHolder holder) {
                 createBackground();
                 createLightning();
-                createSprites();
                 gameLoopThread.setRunning(true);
                 gameLoopThread.start();
             }
@@ -70,23 +74,14 @@ public class GameView extends SurfaceView {
 
     }
 
-    private void createSprites() {
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
-        sprites.add(createSprite(R.drawable.dude_animation_sheet));
+    private Peasant createPeasant(int bitmap) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap assets = BitmapFactory.decodeResource(getResources(), bitmap, options);
+        options.inBitmap = assets;
+        assets = Bitmap.createScaledBitmap(assets, 250,250,true);
+        return new Peasant(this,assets);
     }
 
-    private Sprite createSprite(int resource) {
-        Bitmap bmp = BitmapFactory.decodeResource(getResources(), resource);
-        return new Sprite(this, bmp);
-    }
 
 
     private void createBackground() {
@@ -100,14 +95,58 @@ public class GameView extends SurfaceView {
         lightning = new Lightning(this, bg);
     }
 
+    public void addPeasant(List<Peasant> peasant) {
+        Random random = new Random();
+        float temp = random.nextFloat();
+        systemTime = System.currentTimeMillis();
+        if(peasant.isEmpty()) {
+            peasant.add(createPeasant(R.drawable.good1));
+        }
+
+        else if(temp > difficultyDecider) {
+            peasant.add(createPeasant(R.drawable.good1));
+        }
+
+        if((systemTime - startTime) / timeTracker > 1) {
+            //the longer they survive, the harder it gets
+            //the time tracker is determine the next time where the difficulty will increase
+            timeTracker = timeTracker * 1.3;
+            difficultyDecider = difficultyDecider - .01;
+        }
+        else if((systemTime - startTime) / timeTracker < 0) {
+            //if it is midnight and goes to 12:01 this would be negative
+            //so we need to add on the midnight time in millis
+            systemTime = systemTime + 86400000;
+            if((systemTime - startTime) % timeTracker > 1) {
+                //the longer they survive, the harder it gets
+                //the time tracker is determine the next time where the difficulty will increase
+                timeTracker = timeTracker * 1.3;
+                difficultyDecider = difficultyDecider - .01;
+            }
+
+        }
+
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.WHITE);
         background.onDraw(canvas);
         if (lightning.isVisible())
             lightning.onDraw(canvas);
-        for (Sprite sprite : sprites) {
-            sprite.onDraw(canvas);
+        addPeasant(peasants);
+        synchronized (getHolder()) {
+            for (Peasant peasant : peasants) {
+                if (peasant.getX() < -20) {
+                    //if peasant is off screen then don't render anymore
+                    peasants.remove(peasant);
+                    peasant.bmp.recycle();
+                    peasant.bmp = null;
+                    peasant = null;
+                } else if (peasant.getX() >= 0) {
+                    peasant.onDraw(canvas);
+                }
+            }
         }
         currentScore = activity.getCurrentScore();
         highScore = activity.getHighScore();
@@ -125,10 +164,12 @@ public class GameView extends SurfaceView {
                     lightning.setX(event.getX() - 60);
                     lightning.setVisible(500, activity);
                 }
-                for (int i = sprites.size() - 1; i >= 0; i--) {
-                    Sprite sprite = sprites.get(i);
-                    if (sprite.isCollition(event.getX(), event.getY())) {
-                        sprites.remove(sprite);
+                for (Peasant peasant : peasants) {
+                    if (peasant.isCollition(event.getX(), event.getY())) {
+                        peasants.remove(peasant);
+                        peasant.bmp.recycle();
+                        peasant.bmp = null;
+                        peasant = null;
                         activity.increaseScore();
                         break;
                     }
---
+++ app/src/main/java/cs48/zeusalmighty/Peasant.java
@@ -0,0 +1,75 @@
+package cs48.zeusalmighty;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+
+import java.util.Random;
+
+
+public class Peasant {
+    private static final int BMP_ROWS = 4;
+    private static final int BMP_COLUMNS = 3;
+    private static final int MAX_SPEED = 5;
+    private GameView gameView;
+    public Bitmap bmp;
+    private int x = 0;
+    private int y = 0;
+    private int xSpeed = 15;
+    private int ySpeed;
+    private int currentFrame = 0;
+    private int width;
+    private int height;
+
+    public Peasant(GameView gameView, Bitmap bmp) {
+        this.x = gameView.getWidth();
+        this.y = gameView.getHeight() - 260;
+        this.gameView = gameView;
+        this.bmp = bmp;
+        this.width = bmp.getWidth() / BMP_COLUMNS;
+        this.height = bmp.getHeight() / BMP_ROWS;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    private void update() {
+        if (x > gameView.getWidth() - width - xSpeed) {
+            xSpeed = -15;
+        }
+
+        x = x + xSpeed;
+        currentFrame = ++currentFrame % BMP_COLUMNS;
+    }
+
+    public void onDraw(Canvas canvas) {
+        update();
+        int srcX = currentFrame * width;
+        int srcY = 1 * height;
+        //source the bitmap that the image comes from
+        Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
+        //destination is where on the screen the image will be located
+        Rect dst = new Rect(x, y, x + width, y + height);
+        canvas.drawBitmap(bmp, src, dst, null);
+        src = null;
+        dst = null;
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+
+        return resizedBitmap;
+    }
+
+    public boolean isCollition(float x2, float y2) {
+        return x2 > (x - 100) && x2 < (x + 100 + width) && y2 < 300;
+    }
+}
