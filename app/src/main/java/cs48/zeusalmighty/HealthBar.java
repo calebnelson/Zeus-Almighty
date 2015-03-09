88b03beb8b88f079200ffd5c9b7799c6b60d7dd4
Jared Jensen <jaredbjensen@engineering.ucsb.edu>
2015-03-08 21:29:25 -0700

    added javadoc comments

--- app/src/main/java/cs48/zeusalmighty/HealthBar.java
+++ app/src/main/java/cs48/zeusalmighty/HealthBar.java
@@ -1,19 +1,24 @@
 package cs48.zeusalmighty;
 
+import android.graphics.*;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.Rect;
-import android.graphics.Paint;
-import android.graphics.Color;
+/**
+ *  HealthBar is a class to implement a green bar on the top of the screen that depletes as a user takes hits from enemies.
+ * @author Group 9
+ */
 
 public class HealthBar {
+    
+    // instance variable for bar's color and current health status
     private GameView gameView;
     private Paint healthColor;
     private int x, y;
     public double currentHealth;
 
+    /**
+     * Constructor: sets x and y coordinates for health bar to appear at and initializes health to be full
+     * @param gameView          GameView object that current game is taking place in 
+     */
     public HealthBar(GameView gameView) {
         this.healthColor = new Paint();
         healthColor.setColor(Color.GREEN);
@@ -21,13 +26,20 @@ public class HealthBar {
         this.x = (int)(gameView.getWidth()*.25);
         this.y = 25;
         this.currentHealth = 1.0;
-
     }
 
+    /**
+     * Method to subtract from a player's health whenever a hit is taken from an enemy
+     */
     public void takeHit() {
         currentHealth = currentHealth - 0.1;
     }
 
+    @Override
+    /**
+     * onDraw method to draw health bar at the top of the screen
+     * @param canvas            canvas to be drawn on
+     */
     public void onDraw(Canvas canvas) {
         if(currentHealth > 0) {
             canvas.drawRect(x, y, x + (int) ((gameView.getWidth() * 0.5) * currentHealth), (int) (y + gameView.getHeight() * .05), healthColor);
