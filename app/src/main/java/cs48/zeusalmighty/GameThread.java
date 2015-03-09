08c536e3856da268dec4e33d6e12724b04c1bd16
Jared Jensen <jaredbjensen@engineering.ucsb.edu>
2015-03-08 20:49:03 -0700

    added javadoc comments

--- app/src/main/java/cs48/zeusalmighty/GameThread.java
+++ app/src/main/java/cs48/zeusalmighty/GameThread.java
@@ -1,13 +1,24 @@
 package cs48.zeusalmighty;
 
-
 import android.graphics.Canvas;
 
+/**
+ * GameThread is a cutomized thread used to draw the graphics of the game. By default the thread is set to 
+ * draw at 10 frames per second.
+ * @author Group 9
+ */
+
 public class GameThread extends Thread {
+    
+    //instance variables
     static final long FPS = 10;
     private GameView view;
     private boolean running = false;
 
+    /**
+     * Constructor for GameThread objects
+     * @param view      gameview that thread will be running on
+     */
     public GameThread(GameView view) {
         this.view = view;
     }
@@ -17,6 +28,9 @@ public class GameThread extends Thread {
     }
 
     @Override
+    /**
+     * Draws the graphics for the entire game, updating 10 times per second
+     */
     public void run() {
         long ticksPS = 1000 / FPS;
         long startTime;
