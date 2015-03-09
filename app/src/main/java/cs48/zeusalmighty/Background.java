0d5d585c01c33a1f44507c479416175404ddd5ab
Jared Jensen <jaredbjensen@engineering.ucsb.edu>
2015-03-08 20:43:46 -0700

    Added javadoc comments

--- app/src/main/java/cs48/zeusalmighty/Background.java
+++ app/src/main/java/cs48/zeusalmighty/Background.java
@@ -1,28 +1,40 @@
 package cs48.zeusalmighty;
 
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.Rect;
+import android.graphics.*;
+import java.io.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+/**
+ * Background is a class to hold the background image and the cloud image so that they can easily be drawn at the same time 
+ * with a single call to Background's onDraw() method. 
+ * 
+ * @author Group 9
+ */
 
 public class Background {
 
+    // instance variables
     private GameView gameView;
     private Bitmap bg;
     private Bitmap cloud;
 
+    /**
+     * Constructor to make basic background object
+     * @param gameView      the gameView instance that is currently running
+     * @param bg            a bitmap of the background image
+     * @param cloud         a bitmap of the cloud image
+     */
     public Background(GameView gameView, Bitmap bg, Bitmap cloud) {
         this.gameView = gameView;
         this.bg = getResizedBg(bg, gameView.getHeight(), gameView.getWidth());
         this.cloud = getResizedCloud(cloud, gameView.getHeight(), gameView.getWidth());
     }
 
+    /**
+     * @override
+     * onDraw method to draw the background and cloud images resized properly to the screen size
+     * @param canvas        the canvas to be drawn on
+     */
     public void onDraw(Canvas canvas) {
         Rect src = new Rect(0, 0, bg.getWidth(), bg.getHeight());
         Rect dst = new Rect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -32,11 +44,23 @@ public class Background {
         canvas.drawBitmap(cloud, src2, dst2, null);
     }
 
+    /**
+     * Returns a resized bitmap of the background image that is the same size as the screen being played on
+     * @param bm                background bitmap to resize
+     * @param viewheight        height to resize background to
+     * @param viewwidth         width to resize background to
+     */
     public Bitmap getResizedBg(Bitmap bm, int viewheight, int viewwidth) {
         Bitmap resizedBitmap = Bitmap.createScaledBitmap (bm, viewwidth, viewheight, false);
         return resizedBitmap;
     }
 
+    /**
+     * Returns a resized bitmap of the cloud image that takes up 1/4 of the height of the screen 
+     * @param bm                cloud bitmap to resize
+     * @param viewheight        height to resize cloud to
+     * @param viewwidth         width to resize cloud to
+     */
     public Bitmap getResizedCloud(Bitmap bm, int viewheight, int viewwidth) {
         int height = (int)(viewheight*.25);
         Bitmap resizedBitmap = Bitmap.createScaledBitmap (bm, viewwidth, height, false);
