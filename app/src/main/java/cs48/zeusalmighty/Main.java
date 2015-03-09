5599419753f0e95a7e9d5c130e9159194cd1d819
Jared Jensen <jaredbjensen@engineering.ucsb.edu>
2015-03-08 22:01:42 -0700

    added javadoc comments

--- app/src/main/java/cs48/zeusalmighty/Main.java
+++ app/src/main/java/cs48/zeusalmighty/Main.java
@@ -6,20 +6,28 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.*;
 import android.widget.TextView;
 
+/**
+ * Main is the class that the game actually takes place within. Main is responsible for saving a player's high score and
+ * for initializing the GameView object that will run the game. 
+ * @author Group 9
+ */
 
 public class Main extends Activity {
 
+    // instance variables
     private int currentScore, highScore;
     private MediaPlayer inc, high;
     private GameView gameView;
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
 
+    @Override
+    /**
+    * Creates new GameView object to run game from and loads player's high score to display
+    */
     public void onCreate(Bundle savedInstanceState) {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(savedInstanceState);
@@ -34,20 +42,32 @@ public class Main extends Activity {
         high = MediaPlayer.create(this, R.raw.high);
     }
 
+    /**
+    * returns player's high score
+    */
     public int getHighScore() {
         return highScore;
     }
 
+    /**
+    * returns player's current score
+    */
     public int getCurrentScore() {
         return currentScore;
     }
 
+    /**
+    * increases player's score by one
+    */
     public void increaseScore(){
         this.inc.seekTo(0);
         this.inc.start();
         this.currentScore++;
     }
 
+    /**
+    * Saves player's score if it is higher than current high score
+    */
     public void saveScore() {
         if (this.currentScore > this.highScore) {
             this.highScore = this.currentScore;
@@ -56,7 +76,6 @@ public class Main extends Activity {
             editor.putInt("High Score", this.highScore);
             editor.commit();
         }
-        //currentScore = 0;
     }
 
 }
