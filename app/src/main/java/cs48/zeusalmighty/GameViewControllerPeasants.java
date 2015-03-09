952f0dac4a3cdb1f579e50898db610f42f688176
Jared Jensen <jaredbjensen@engineering.ucsb.edu>
2015-03-08 22:42:43 -0700

    added javadoc comments

--- app/src/main/java/cs48/zeusalmighty/GameViewControllerPeasants.java
+++ app/src/main/java/cs48/zeusalmighty/GameViewControllerPeasants.java
@@ -1,40 +1,36 @@
 package cs48.zeusalmighty;
 
+import android.content.*;
+import android.graphics.*;
+import android.view.*;
+import java.util.*;
+
 /**
- * Created by Po on 3/5/15.
+ * Controller class for the peasant enemies present in the game. This class is responsible for drawing, creating, and
+ * destroying peasant enemies during the course of gameplay.
+ * @author Group 9
  */
-
-
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
-
 
 public class GameViewControllerPeasants {
 
+    // instance variables
     private Main activity;
     private GameView gameView;
 
-
+    /**
+    * Default constructor
+    * @param gameView       GameView object currently running game
+    * @param activity       Main activity running game
+    */
     public GameViewControllerPeasants(GameView gameView, Main activity) {
         this.activity = activity;
         this.gameView = gameView;
     }
 
-
+    /**
+    * Creates a peasant enemy from a given bitmap to be added to the list of peasants in the game
+    * @param bitmap         bitmap used to draw peasant enemy
+    */
     private Peasant createPeasant(int bitmap) {
         BitmapFactory.Options options = new BitmapFactory.Options();
         Bitmap assets = BitmapFactory.decodeResource(gameView.getResources(), bitmap);
@@ -44,7 +40,12 @@ public class GameViewControllerPeasants {
         return new Peasant(gameView, assets);
     }
 
-
+    /**
+    * Adds a new peasant enemy to the list of peasants in the GameView. Checks if the player has health before adding a new
+    * peasant to the list.
+    * @param peasant        arrayList of peasant to be added to 
+    * @param healthBar      health bar from game to check to make sure player is still alive
+    */
     public void addPeasant(List<Peasant> peasant, HealthBar healthBar) {
         Random random = new Random();
         float temp = random.nextFloat();
@@ -75,6 +76,13 @@ public class GameViewControllerPeasants {
         }
     }
 
+    /**
+    * method to call from onDraw method of GameView. iterates over the list of peasants in the game and draws each one that 
+    * has not yet reached the edge of the screen
+    * @param peasants       list of peasants currently in the game
+    * @param healthBar      health bar object from game
+    * @param canvas         canvas to be drawn on
+    */
     public void drawPeasant(List<Peasant> peasants, HealthBar healthBar, Canvas canvas) {
         Iterator<Peasant> it = peasants.iterator();
         while (it.hasNext()) {
@@ -92,11 +100,14 @@ public class GameViewControllerPeasants {
         }
     }
 
-
-
-
+    /**
+    * Method to determine if any peasants were hit based on a given lightning bolt's location
+    * @param peasants       list of peasant enemies in the game currently
+    * @param x              x coordinate of lightning bolt
+    * @param y              y coordinate of lightning bolt
+    * @param boom           bitmap of explosion graphic to display after a kill
+    */
     public void peasantHit(List<Peasant> peasants, float x, float y, Bitmap boom) {
-
 
         Iterator<Peasant> it = peasants.iterator();
         while (it.hasNext()) {
@@ -120,7 +131,6 @@ public class GameViewControllerPeasants {
                 gameView.removed = 1;
             }
         }
-
 
     }
 
